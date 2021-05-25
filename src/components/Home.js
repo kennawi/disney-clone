ea94fe01232b3f9a3770c8fc2570b76e982f82cf
Kennawi <ahmedelkenawy222@gmail.com>
2021-05-25 21:33:57 +0200

    Add Viewers

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 import ImgSlider from './ImgSlider'
+import Viewers from './Viewers'
 
 
 function Home() {
     return (
         <Container>
             <ImgSlider/>
+            <Viewers/>
         </Container>
     )
 }
