5888972d97c0548bfd26af8e6363e708964a8745
Kennawi <ahmedelkenawy222@gmail.com>
2021-05-25 20:56:38 +0200

    added slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import ImgSlider from './ImgSlider'
+
 
 function Home() {
     return (
         <Container>
-            Home
+            <ImgSlider/>
         </Container>
     )
 }
 
 export default Home
+
+
  const Container = styled.main`
     min-height: calc(100vh - 70px);
     padding: 0 calc(3.5vw + 5px);
     position: relative;
+    overflow-x: hidden;
 
     &:before {
         background:url("/images/home-background.png") center center / cover 
--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import Slider from "react-slick";
-import "~slick-carousel/slick/slick.css"; 
-import "~slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 function ImgSlider() {
     let settings = {
@@ -11,12 +11,15 @@ function ImgSlider() {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autuplay:true,
+        autoplay:true
     }
     return (
         <Carousel {...settings}>
             <Warp>
                 <img src='/images/slider-badging.jpg'/>
+            </Warp>
+            <Warp>
+                <img src='/images/slider-badag.jpg'/>
             </Warp>
         </Carousel>
     )
@@ -26,7 +29,38 @@ export default ImgSlider
 
 const Carousel = styled(Slider)`
 
+    ul li button {
+        &:before{
+            font-size: 10px;
+            color: rgb(150,158,171);
+        }
+    }
+
+    line-height.slick-active bottom::before {
+        color:white;
+    }
+    .slick-list {
+        overflow: visible;
+    }
+
+    button{
+        z-index: 1;
+    }
+    
 `;
 const Warp = styled.div`
+    cursor: pointer;
+    img {
+        border: 4px solid transparent;
+        border-radius: 4px;
+        width:100%;
+        height:100%;
+        box-shadow: rgb(0 0 0/ 69%) 0px 26px 30px -10px,
+        rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+        transition-duration:300ms;
 
+        &:hover {
+            border: 4px solid rgba(249,249,249,0.8);
+        }
+    }
 `;
